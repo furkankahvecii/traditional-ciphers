5395c5b248347a869819ffeacfc0a718df28ac9c
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2019-05-19 02:23:32 +0300

    Update alphabetsMatrix

--- Bifid Cipher/BifidCipher.java
+++ Bifid Cipher/BifidCipher.java
@@ -1,8 +1,8 @@
 
 public class BifidCipher {
 	
-	static char alphabetsMatrix[][] = {  {'A','B','C','D','E'} , {'F','G','H','I','K'} , {'L','M','N','O','P'} , 
-		       						   	 {'Q','R','S','T','U'} , {'V','W','X','Y','Z'} };
+	static char alphabetsMatrix[][] = {  {'A','B','C','D','E','F'} , {'G','H','I','J','K','L'} , {'M','N','O','P','Q','R'} , 
+		       							 {'S','T','U','V','W','X'} , {'Y','Z','0','1','2','3'} , {'4','5','6','7','8','9'} };
 	// Sifreleme metodu
 	public static String encryption(String plainText)
 	{
